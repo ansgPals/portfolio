60a25587463573ccab4d9c6aa4b7243ce62135c7
hyemin.mun <iamm71kr@gmail.com>
2025-06-16 19:05:09 +0900

    포트폴리오

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,10 +13,6 @@ function MyApp({ Component, pageProps }: AppProps) {
           name="description"
           content="프론트엔드 개발자 문혜민의 포트폴리오입니다."
         />
-        <meta
-          name="keywords"
-          content="문혜민, 프론트엔드, React, Next.js, 포트폴리오, 웹개발자"
-        />
         <meta name="author" content="문혜민" />
 
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -27,10 +23,13 @@ function MyApp({ Component, pageProps }: AppProps) {
           content="프론트엔드 개발자 문혜민입니다."
         />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://your-domain.com" />
+        <meta
+          property="og:url"
+          content="https://portfolio-opal-iota-92.vercel.app"
+        />
         <meta
           property="og:image"
-          content="https://your-domain.com/og-image.png"
+          content="https://portfolio-opal-iota-92.vercel.app/idea.png"
         />
 
         <link rel="icon" href="/favicon.ico" />
